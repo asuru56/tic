eabdf01a011dfda96bfdda9dd9f3ab9dce2cda6f
asuru56 <56asuru@gmail.com>
2024-01-29 09:14:52 +0530

    Started Work on Winner Game()

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let flag = 1;
+let flag = 1; 
 
+function check(){
+    for (let i = 0; i < 9; i++) {
+    const block = document.querySelectorAll('.block')[i];
+    if(block.innerText == 'X'){
+        console.log(block,"is X");
+
+    }
+    if(block.innerText == 'O'){
+        console.log(block,"is O");
+        
+    }
+}
+function checkForWinner() {
+    for(let i = 0 ; i < 9 ; i++){
+        const block = document.querySelectorAll('.block')[i];
+        if(block.innerText == "X"){
+
+        }
+    }
+}
+      
+   
+}
 // Add event listeners for all blocks
 for (let i = 0; i < 9; i++) {
     const block = document.querySelectorAll('.block')[i];
@@ -9,10 +32,13 @@ for (let i = 0; i < 9; i++) {
             // Set 'X' if the block is empty and it's player 1's turn
             block.innerText = 'X';
             flag = 0; // Switch to player 2's turn
+            check()
         } else if (flag === 0 && block.innerText === '') {
             // Set 'O' if the block is empty and it's player 2's turn
             block.innerText = 'O';
             flag = 1; // Switch to player 1's turn
+            check()
         }
     });
 }
+
