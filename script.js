d3fb895e2f3d22782328a41bf4d0804fed04d970
asuru56 <56asuru@gmail.com>
2024-01-29 10:50:26 +0530

    Added XO Validation

--- script.js
+++ script.js
@@ -11,18 +11,31 @@ function check(){
         console.log(block,"is O");
         
     }
+    return checkForWinner() ; 
 }
-function checkForWinner() {
-    for(let i = 0 ; i < 9 ; i++){
-        const block = document.querySelectorAll('.block')[i];
-        if(block.innerText == "X"){
+}
 
+function checkForWinner() {
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+
+    for (let combo of winningCombos) {
+        const [a, b, c] = combo;
+        const blocks = document.querySelectorAll('.block');
+        if (blocks[a].innerText && blocks[a].innerText === blocks[b].innerText && blocks[a].innerText === blocks[c].innerText) {
+            // We have a winner
+            return blocks[a].innerText;
         }
     }
+
+    return null; // No winner yet
 }
       
    
-}
+ 
 // Add event listeners for all blocks
 for (let i = 0; i < 9; i++) {
     const block = document.querySelectorAll('.block')[i];
